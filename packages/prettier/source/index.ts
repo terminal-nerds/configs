260e5fb85f9f500442bbe14ca2e95ba0c7b59c36
Matt Kadlubowski <xeho91@pm.me>
2022-01-01 09:56:44 +0800

    feat(Svelte): ✨  Add options for Prettier plugin

--- packages/prettier/source/index.ts
+++ packages/prettier/source/index.ts
@@ -3,7 +3,10 @@ import { deepmerge } from "deepmerge-ts";
 import { hasModule } from "@workspace/helpers";
 
 import prettierDefault from "./prettier.js";
+import pluginSvelte from "./extensions/svelte.js";
 
-const configurations = deepmerge(prettierDefault);
+const configurations = deepmerge(prettierDefault, {
+	...hasModule("svelte") && pluginSvelte,
+});
 
 module.exports = configurations;
