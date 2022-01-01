a9b3bed266a2ff0d845f85e4558b2fccf828fa0b
Matt Kadlubowski <xeho91@pm.me>
2022-01-01 10:02:30 +0800

    build: 🔧  Add `pretty-quick` & use package config

--- packages/prettier/source/index.ts
+++ packages/prettier/source/index.ts
@@ -6,7 +6,7 @@ import prettierDefault from "./prettier.js";
 import pluginSvelte from "./extensions/svelte.js";
 
 const configurations = deepmerge(prettierDefault, {
-	...hasModule("svelte") && pluginSvelte,
+	...(hasModule("svelte") && pluginSvelte),
 });
 
 module.exports = configurations;
