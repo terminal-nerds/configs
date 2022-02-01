f317d8244837285add6c9554695379180d2adaa9
Matt Kadlubowski <xeho91@pm.me>
2022-02-01 20:17:18 +0800

    build(lint-staged): 🔧  Add initial config

---
+++ .lintstagedrc.js
@@ -0,0 +1,26 @@
+// https://github.com/okonet/lint-staged
+
+const config = {
+	// Prettier
+	"*": ["pretty-quick --check --staged"],
+
+	// ESLint
+	"*.{js,json,ts,yml,yaml}": ["eslint"],
+
+	// markdownlint
+	"*.md": ["markdownlint"],
+
+	// TypeScript types (in each package, because of different configs)
+	"./helpers/**/*.ts": () =>
+		`tsc --project ./helpers/tsconfig.json --noEmit`,
+	"./packages/eslint/source/**/*.ts": () =>
+		`tsc --project ./packages/eslint/tsconfig.json --noEmit`,
+	"./packages/markdownlint/source/**/*.ts": () =>
+		`tsc --project ./packages/markdownlint/tsconfig.json --noEmit`,
+	"./packages/prettier/source/**/*.ts": () =>
+		`tsc --project ./packages/prettier/tsconfig.json --noEmit`,
+	"./packages/stylelint/source/**/*.ts": () =>
+		`tsc --project ./packages/stylelint/tsconfig.json --noEmit`,
+};
+
+export default config;
