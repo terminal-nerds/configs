b965e92e8baeb2c4df3bf38a7dbec8ffc5f20483
Matt Kadlubowski <xeho91@pm.me>
2022-02-12 16:17:35 +0800

    feat(syncpack): ✨  Add new configuration package
    Resolves #42

---
+++ packages/syncpack/source/index.ts
@@ -0,0 +1,50 @@
+import type { SyncpackConfig } from "syncpack/dist/constants";
+
+const config: Partial<SyncpackConfig> = {
+	// https://github.com/JamieMason/syncpack/#-configuration-file
+	dev: true,
+	filter: ".",
+	indent: "\t",
+	peer: true,
+	prod: true,
+	semverRange: "^",
+	sortAz: [
+		"engines",
+		"files",
+		"peerDependencies",
+		"dependencies",
+		"devDependencies",
+	],
+	sortFirst: [
+		"$schema",
+		"private",
+		"type",
+		"publishConfig",
+		"name",
+		"version",
+		"description",
+		"keywords",
+		"author",
+		"license",
+		"homepage",
+		"repository",
+		"bugs",
+		"engines",
+		"volta",
+		"main",
+		"files",
+		"exports",
+		"typesVersions",
+		"turbo",
+		"ultra",
+		"scripts",
+		"peerDependencies",
+		"dependencies",
+		"devDependencies",
+	],
+	source: ["package.json", "packages/*/package.json"],
+	versionGroups: [],
+};
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = config;
