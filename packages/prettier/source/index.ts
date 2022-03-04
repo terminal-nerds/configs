7e5e82718787bc75d85c8ed25c077c5064432352
Matt Kadlubowski <xeho91@pm.me>
2022-03-04 20:55:47 +0800

    feat(prettier-config): 🏗️ Replace `deepmerge-ts` with helper - `createMergedConfig()`

--- packages/prettier/source/index.ts
+++ packages/prettier/source/index.ts
@@ -1,13 +1,17 @@
-import { deepmerge } from "deepmerge-ts";
+import { createMergedConfig } from "@workspace/helpers/configuration";
+import { hasModule } from "@workspace/helpers/module";
 
 import prettier from "./prettier.js";
 
 import pluginSvelte from "./plugins/svelte";
 
-const configurations = deepmerge(
+const mergedConfig = createMergedConfig([
+	// Base
 	prettier,
 
-	pluginSvelte,
-);
+	// Plugins
+	hasModule("svelte") && pluginSvelte,
+]);
 
-module.exports = configurations;
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = mergedConfig;
