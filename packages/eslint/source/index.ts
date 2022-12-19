90c95956705fa961bdba1a04ef78d9ca5f06f9e7
mateusz.k <mateusz.k@adchitects.co>
2022-12-19 23:32:16 +0100

    feat(package/eslint): 🔧  Implement new plugins configs

--- packages/eslint/source/index.ts
+++ packages/eslint/source/index.ts
@@ -6,6 +6,7 @@ import eslint from "./eslint.js";
 
 import pluginCompat from "./plugins/compat.js";
 import pluginDiff from "./plugins/diff.js";
+import pluginEmotion from "./plugins/emotion.js";
 import pluginImport from "./plugins/import.js";
 import pluginJest from "./plugins/jest.js";
 import pluginJestDOM from "./plugins/jest-dom.js";
@@ -14,6 +15,7 @@ import pluginJSONC from "./plugins/jsonc.js";
 import pluginJSONSchemaValidator from "./plugins/json-schema-validator.js";
 import pluginJSXA11y from "./plugins/jsx-a11y.js";
 import pluginNode from "./plugins/node.js";
+import pluginSimpleImportSort from "./plugins/simple-import-sort.js";
 import pluginSonarJS from "./plugins/sonarjs.js";
 import pluginReact from "./plugins/react.js";
 import pluginReactHooks from "./plugins/react-hooks.js";
@@ -21,6 +23,7 @@ import pluginRegexp from "./plugins/regexp.js";
 import pluginSQL from "./plugins/sql.js";
 import pluginStorybook from "./plugins/storybook.js";
 import pluginSvelte3 from "./plugins/svelte3.js";
+import pluginTailwindCSS from "./plugins/tailwindcss.js";
 import pluginTestingLibrary from "./plugins/testing-library.js";
 import pluginTypeScript from "./plugins/typescript.js";
 import pluginUnicorn from "./plugins/unicorn.js";
@@ -36,6 +39,7 @@ const mergedConfig = createMergedConfig([
 	// Plugins
 	pluginCompat,
 	isContinuousIntegration() && pluginDiff,
+	(hasModule("@emotion/css") || hasModule("@emotion/react")) && pluginEmotion,
 	pluginImport,
 	(hasModule("jest") || hasModule("vitest")) && pluginJest,
 	hasModule("@testing-library/jest-dom") && pluginJestDOM,
@@ -47,18 +51,20 @@ const mergedConfig = createMergedConfig([
 	(hasModule("react") || hasModule("preact")) && pluginReact,
 	(hasModule("react") || hasModule("preact")) && pluginReactHooks,
 	pluginRegexp,
+	pluginSimpleImportSort,
 	pluginSonarJS,
 	pluginSQL,
 	hasModule("sb") && pluginStorybook,
 	hasModule("svelte") && pluginSvelte3,
 	hasModule("@testing-library/jest-dom") && pluginTestingLibrary,
+	hasModule("tailwindcss") && pluginTailwindCSS,
 	hasModule("typescript") && pluginTypeScript,
 	pluginUnicorn,
 	pluginYML,
 
 	// Configs
-	// NOTE: Must come as last!
 	hasModule("next") && configNext,
+	// NOTE: Must come as last!
 	hasModule("prettier") && configPrettier,
 ]);
 
