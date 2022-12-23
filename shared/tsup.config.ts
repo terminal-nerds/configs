5414f7e13848dd543d64d069fc6ab8f3c69ee562
xeho91 <xeho91@pm.me>
2022-12-23 23:07:38 +0100

    refactor(shared): 🏗  Create exports in CJS formats

--- shared/tsup.config.ts
+++ shared/tsup.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
+export default defineConfig((options) => ({
+	bundle: true,
 	clean: true,
 	dts: true,
 	entry: ["source/*.ts"],
-	format: ["esm"],
+	format: ["cjs", "esm"],
 	minify: true,
+	noExternal: ["find-up", "locate-path", "read-pkg", "read-pkg-up"],
 	outDir: "dist/",
+	shims: true,
+	skipNodeModulesBundle: true,
 	sourcemap: true,
-	splitting: true,
-	target: ["node16"],
-});
+	splitting: false,
+	treeshake: true,
+}));
