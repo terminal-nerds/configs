94d94b90389bdd059c32900345d228d62e6c0c32
xeho91 <xeho91@pm.me>
2022-12-27 22:32:47 +0100

    refactor(eslint): 🏗  Apply the `shared` changes & move to ESM

--- packages/eslint/tsup.config.ts
+++ packages/eslint/tsup.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "tsup";
 
+import { dependencies } from "./package.json";
+
 export default defineConfig({
+	bundle: true,
+	dts: true,
 	clean: true,
 	entry: ["source/index.ts"],
+	external: Object.keys(dependencies),
+	noExternal: ["@workspace/shared"],
 	format: ["cjs"],
 	minify: true,
-	noExternal: ["@workspace/shared"],
 	sourcemap: true,
 	splitting: false,
 	target: ["node16"],
+	treeshake: true,
 });
