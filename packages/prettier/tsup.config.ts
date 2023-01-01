5e2c76e34dcc85aa79cd04d7e19f709eaabf3f38
xeho91 <xeho91@pm.me>
2023-01-01 21:01:42 +0100

    refactor(prettier): ♻️  Implement reusable `tsup` config

--- packages/prettier/tsup.config.ts
+++ packages/prettier/tsup.config.ts
@@ -1,18 +1,12 @@
+import { getNodeCJSOptions } from "@terminal-nerds/tsup-config";
 import { defineConfig } from "tsup";
 
 import { dependencies } from "./package.json";
 
-export default defineConfig({
+export default defineConfig((options) => ({
+	...getNodeCJSOptions(options),
 	bundle: true,
-	dts: true,
-	clean: true,
 	entry: ["source/index.ts"],
 	external: Object.keys(dependencies),
 	noExternal: ["@workspace/shared"],
-	format: ["cjs"],
-	minify: true,
-	sourcemap: true,
-	splitting: false,
-	target: ["node16"],
-	treeshake: true,
-});
+}));
