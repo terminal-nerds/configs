7ad0a66513fcafbba4d643cbab3987b59d2c34e5
xeho91 <xeho91@pm.me>
2023-01-01 21:22:51 +0100

    chore(packages): 🔧  Improve `tsup` configs - remove `external`

--- packages/stylelint/tsup.config.ts
+++ packages/stylelint/tsup.config.ts
@@ -1,12 +1,9 @@
 import { getNodeCJSOptions } from "@terminal-nerds/tsup-config";
 import { defineConfig } from "tsup";
 
-import { dependencies } from "./package.json";
-
 export default defineConfig((options) => ({
 	...getNodeCJSOptions(options),
 	bundle: true,
 	entry: ["source/index.ts"],
-	external: Object.keys(dependencies),
 	noExternal: ["@workspace/shared"],
 }));
