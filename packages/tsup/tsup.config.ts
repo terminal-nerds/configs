856f9793bce06a2358a4ecc2623ffabdfb7c9e18
xeho91 <xeho91@pm.me>
2023-01-01 20:28:36 +0100

    chore(tsup): ✨  Add initial configuration getters

---
+++ packages/tsup/tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tsup";
+
+import { getNodeUniveralOptions } from "./source/main.js";
+
+export default defineConfig((options) => ({
+	...getNodeUniveralOptions(options),
+	entry: ["./source/**/*.ts"],
+}));
