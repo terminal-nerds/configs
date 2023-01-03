b53a85343effcfd724e3d40622b47623b6323a75
xeho91 <xeho91@pm.me>
2023-01-03 20:40:17 +0100

    feat(browserslist): ✨  Add initial config files

---
+++ packages/browserslist/source/browsers.ts
@@ -0,0 +1,4 @@
+// https://browsersl.ist
+const config = ["defaults and supports es6-module"];
+
+export default config;
---
+++ packages/browserslist/source/node.ts
@@ -0,0 +1,11 @@
+// https://browsersl.ist
+const config = [
+	/**
+	 * NOTE:
+	 * Meaning LTS version.
+	 * Any version above LTS is not considered as "major release", AFAIK.
+	 */
+	"last 1 node major versions",
+];
+
+export default config;
