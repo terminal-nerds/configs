6a6e931902aea76ec2643184077c7b3a948e5d39
xeho91 <xeho91@pm.me>
2023-01-08 18:59:24 +0100

    chore(lint-staged): 🔧  Reuse the shared config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,30 +1,8 @@
+import { CONFIG } from "@terminal-nerds/lint-staged-config";
+
 // https://github.com/okonet/lint-staged
-
 const config = {
-	// Prettier
-	"*": ["pretty-quick --check --staged"],
-
-	// ESLint
-	"*.{js,json,ts,yml,yaml}": ["eslint"],
-
-	// depcheck & syncpack
-	"**/package.json": () => [
-		"depcheck",
-		"syncpack list-mismatches",
-		"syncpack format",
-	],
-
-	// markdownlint
-	"*.md": [`markdownlint --ignore "./.changeset/*.md"`],
-
-	// TypeScript types (in each package, because of different configs)
-	"./shared/**/*.ts": () => `tsc --project ./shared/tsconfig.json --noEmit`,
-	"./packages/eslint/source/**/*.ts": () =>
-		`tsc --project ./packages/eslint/tsconfig.json --noEmit`,
-	"./packages/prettier/source/**/*.ts": () =>
-		`tsc --project ./packages/prettier/tsconfig.json --noEmit`,
-	"./packages/stylelint/source/**/*.ts": () =>
-		`tsc --project ./packages/stylelint/tsconfig.json --noEmit`,
+	...CONFIG,
 };
 
 export default config;
