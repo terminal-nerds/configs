928dc5820e759947f79acf68bbcef5c6a64b12fa
xeho91 <xeho91@pm.me>
2023-01-08 18:25:38 +0100

    feat(shared): ✨  Add `getNearestTypeScriptConfigPath()`

--- shared/source/project.ts
+++ shared/source/project.ts
@@ -1,4 +1,5 @@
 import { existsSync } from "node:fs";
+import { join } from "node:path";
 
 import { packageDirectorySync } from "pkg-dir";
 
@@ -9,5 +10,16 @@ export function getNearestPackageRootPath(currentPath: string): string {
 		return packageDirectoryPath;
 	} else {
 		throw new Error(`Cannot determine the nearest root of the package for the file: ${currentPath}!`);
+	}
+}
+
+export function getNearestTypeScriptConfigPath(currentPath: string): string {
+	const packageRootPath = getNearestPackageRootPath(currentPath);
+	const configPath = join(packageRootPath, "tsconfig.json");
+
+	if (existsSync(configPath)) {
+		return configPath;
+	} else {
+		throw new Error('Cannot locate nearest "tsconfig.json" file!');
 	}
 }
