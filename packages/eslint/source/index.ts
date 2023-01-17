6d6fcae5b0facdcde6a4ff2b04d49854c4514edf
xeho91 <xeho91@pm.me>
2023-01-17 21:48:06 +0800

    fix(ESLint): 🔧  Remove CI condition to load plugin

--- packages/eslint/source/index.ts
+++ packages/eslint/source/index.ts
@@ -1,5 +1,4 @@
 import { createMergedConfig } from "@workspace/shared/configuration";
-import { isContinuousIntegration } from "@workspace/shared/environment";
 import { hasPackage } from "@workspace/shared/package";
 import type { ESLintConfig } from "eslint-define-config";
 
@@ -39,7 +38,7 @@ const config = createMergedConfig<SimplifiedESLintConfig>([
 
 	// Plugins
 	pluginCompat,
-	isContinuousIntegration() && pluginDiff,
+	pluginDiff,
 	(hasPackage("@emotion/css") || hasPackage("@emotion/react")) && pluginEmotion,
 	pluginImport,
 	(hasPackage("jest") || hasPackage("vitest")) && pluginJest,
--- packages/eslint/source/plugins/diff.ts
+++ packages/eslint/source/plugins/diff.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "eslint-define-config";
 
+const CONFIG_TO_EXTEND = [];
+
+if (process.env["ESLINT_PLUGIN_DIFF_ENABLE"] === "true") {
+	CONFIG_TO_EXTEND.push("plugin:diff/diff");
+}
+
 // https://github.com/paleite/eslint-plugin-diff
 const config = defineConfig({
-	extends: ["plugin:diff/diff"],
+	extends: CONFIG_TO_EXTEND,
 });
 
 export default config;
