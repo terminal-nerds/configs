cd0612a81b3569cfa6f994d672972547dcf68eb2
xeho91 <xeho91@pm.me>
2023-01-19 21:51:00 +0800

    feat(shared/environment): ✨  Add `IN_BROWSER` and `IN_NODE_JS` constants

--- shared/source/environment.ts
+++ shared/source/environment.ts
@@ -1,3 +1,6 @@
+export const IN_BROWSER = globalThis.window !== undefined;
+export const IN_NODE_JS = globalThis.process !== undefined;
+
 export function isContinuousIntegration(): boolean {
 	const environmentVariables = Object.keys(process.env);
 	const names = new Set(["CI", "CONTINUOUS_INTEGRATION"]);
