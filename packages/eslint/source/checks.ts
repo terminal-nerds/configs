729f610cbd399c3eca64d098368a1dcbe5b35c40
xeho91 <xeho91@pm.me>
2023-01-19 23:20:36 +0800

    perf(eslint): ⚡️  Add separate file to do the module checks once

---
+++ packages/eslint/source/checks.ts
@@ -0,0 +1,13 @@
+import { hasModule } from "@workspace/shared/module";
+
+export const HAS_DOCUSAURUS = hasModule("@docusaurus/core");
+export const HAS_EMOTION = hasModule("@emotion/css") || hasModule("@emotion/react");
+export const HAS_JEST = hasModule("jest") || hasModule("vitest");
+export const HAS_JEST_DOM = hasModule("@testing-library/jest-dom");
+export const HAS_NEXT = hasModule("next");
+export const HAS_PRETTIER = hasModule("prettier");
+export const HAS_REACT = hasModule("preact") || hasModule("react");
+export const HAS_STORYBOOK = hasModule("storybook");
+export const HAS_SVELTE = hasModule("svelte");
+export const HAS_TAILWINDCSS = hasModule("tailwindcss");
+export const HAS_TYPESCRIPT = hasModule("typescript");
