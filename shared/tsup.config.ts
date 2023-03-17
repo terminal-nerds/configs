d852ad5f399bec6671f7a105be8075073d9808d1
xeho91 <xeho91@pm.me>
2023-03-17 17:52:31 +0800

    build(shared): 🔥  Remove `shared/` package

--- shared/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { getNodeUniveralOptions } from "@terminal-nerds/tsup-config";
-import { defineConfig } from "tsup";
-
-export default defineConfig((options) => ({
-	...getNodeUniveralOptions(options),
-	bundle: true,
-	entry: ["source/*.ts"],
-}));
