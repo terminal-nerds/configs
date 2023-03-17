060e6d905019eb9f3aeda6d14007c22031e1780e
xeho91 <xeho91@pm.me>
2023-03-18 00:21:38 +0800

    chore(Vitest): 🔧  Enable `logHeapUsage`

--- packages/vitest/source/base.ts
+++ packages/vitest/source/base.ts
@@ -4,6 +4,7 @@ export const IS_WATCH_COMMAND = process.argv[2] === "watch";
 
 // https://vitest.dev/config/
 export const BASE_OPTIONS: UserConfig["test"] = {
+	logHeapUsage: true,
 	open: false,
 	ui: IS_WATCH_COMMAND,
 };
