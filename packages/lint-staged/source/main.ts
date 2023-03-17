c10b2e711a384a8836f320bd2912d84193caad0c
xeho91 <xeho91@pm.me>
2023-03-18 00:35:45 +0800

    feat(lint-staged): ✨  Add support for running tests with `vitest`

--- packages/lint-staged/source/main.ts
+++ packages/lint-staged/source/main.ts
@@ -6,17 +6,17 @@ import {
 	HAS_STYLELINT,
 	HAS_SYNCPACK,
 	HAS_TYPESCRIPT,
+	HAS_VITEST,
 } from "./checks.js";
 import { CODE_FORMAT } from "./groups/code-format.js";
 import { ESLINT } from "./groups/eslint.js";
 import { MARKDOWN } from "./groups/markdown.js";
 import { PACKAGE_JSON } from "./groups/package-json.js";
 import { STYLESHEETS } from "./groups/stylesheets.js";
+import { TESTS } from "./groups/tests.js";
 import { TYPESCRIPT } from "./groups/typescript.js";
 
-/**
- * {@link https://github.com/okonet/lint-staged#configuration}
- */
+/** {@link https://github.com/okonet/lint-staged#configuration} */
 export const CONFIG = {
 	...(HAS_PRETTIER && CODE_FORMAT),
 	...(HAS_ESLINT && ESLINT),
@@ -24,4 +24,5 @@ export const CONFIG = {
 	...((HAS_DEPCHECK || HAS_SYNCPACK) && PACKAGE_JSON),
 	...(HAS_STYLELINT && STYLESHEETS),
 	...(HAS_TYPESCRIPT && TYPESCRIPT),
+	...(HAS_VITEST && TESTS),
 };
