c930e529eac79465146f31bd68c6693f36bf250a
xeho91 <xeho91@pm.me>
2023-03-28 18:41:45 +0800

    fix(ESLint): 🐛  Fix missing `@` scope tag in extending `@unocss` config

---
+++ .changeset/strong-spoons-think.md
@@ -0,0 +1,5 @@
+---
+"@terminal-nerds/eslint-config": patch
+---
+
+🐛 Fix missing `@` scope tag in extending `@unocss` config
--- packages/eslint/source/configs/unocss.ts
+++ packages/eslint/source/configs/unocss.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "eslint-define-config";
 
 /** @see {@link https://github.com/unocss/unocss/tree/main/packages/eslint-config} Config repository */
 const config = defineConfig({
-	extends: ["unocss"],
+	extends: ["@unocss"],
 });
 
 export default config;
