525e5065b36837422bfd44ce6abe634ad6d98981
xeho91 <xeho91@pm.me>
2023-04-04 22:54:25 +0800

    feat(ESLint): 🔧  Disable plugin `json-schema-validator` in CI & CD

---
+++ .changeset/tough-chicken-appear.md
@@ -0,0 +1,5 @@
+---
+"@terminal-nerds/eslint-config": minor
+---
+
+🔧 Disable `json-schema-validator` plugin, when running in CI & CD
--- packages/eslint/source/checks.ts
+++ packages/eslint/source/checks.ts
@@ -1,4 +1,5 @@
 import { hasModule } from "@terminal-nerds/snippets-runtime/module";
+import { isIn } from "@terminal-nerds/snippets-runtime/scope";
 
 export const HAS_DOCUSAURUS = hasModule("@docusaurus/core");
 export const HAS_EMOTION = hasModule("@emotion/css") || hasModule("@emotion/react");
@@ -12,3 +13,5 @@ export const HAS_SVELTE = hasModule("svelte");
 export const HAS_TAILWINDCSS = hasModule("tailwindcss");
 export const HAS_TYPESCRIPT = hasModule("typescript");
 export const HAS_UNOCSS = hasModule("unocss");
+
+export const IS_IN_CI = isIn("continuousIntegration", { strict: true });
