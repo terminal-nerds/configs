c395b126325ccfae8b426bafed76cebb60a41e77
Mateusz Kadlubowski <xeho91@pm.me>
2023-04-05 17:14:49 +0800

    feat(Prettier): 🔧  Tweak loading conditions for `jsdoc` plugin (#301)

--- packages/prettier/source/checks.ts
+++ packages/prettier/source/checks.ts
@@ -1,5 +1,12 @@
 import { hasModule } from "@terminal-nerds/snippets-runtime/module";
+import { getEnvironmentVariable } from "@terminal-nerds/snippets-runtime/variable";
 
 export const HAS_SVELTE = hasModule("svelte");
 export const HAS_TAILWINDCSS = hasModule("tailwindcss");
 export const HAS_TYPESCRIPT = hasModule("typescript");
+
+export type DocumentationType = "jsdoc" | "tsdoc";
+export const DOCUMENTATION_TYPE = getEnvironmentVariable<DocumentationType>("DOCUMENTATION_TYPE");
+
+/** Temporary workaround. */
+export const DISABLE_JSDOC_PARSER = getEnvironmentVariable<boolean>("DISABLE_JSDOC_PARSER");
--- packages/prettier/source/plugins/jsdoc.ts
+++ packages/prettier/source/plugins/jsdoc.ts
@@ -1,20 +1,20 @@
-import { HAS_TYPESCRIPT } from "../checks.ts";
+import { DISABLE_JSDOC_PARSER, DOCUMENTATION_TYPE, HAS_TYPESCRIPT } from "../checks.ts";
 
-/** {@link https://github.com/hosseinmd/prettier-plugin-jsdoc} Plugin JSDoc options */
+/** {@link https://github.com/hosseinmd/prettier-plugin-jsdoc} Plugin JSDoc options. */
 const config = {
 	plugins: ["prettier-plugin-jsdoc"],
 
 	/**
-	 * FIXME: Enable it once this issue is resolved, because is hard to work with it:
-	 * https://github.com/hosseinmd/prettier-plugin-jsdoc/issues/183
+	 * FIXME: Be wary of this issue, ecause is hard to work with it:
+	 * https://github.com/hosseinmd/prettier-plugin-jsdoc/issues/183.
 	 */
-	jsdocParser: !HAS_TYPESCRIPT,
+	jsdocParser: !DISABLE_JSDOC_PARSER,
 	jsdocSpaces: 1,
 	// jsdocPrintWidth: number,
-	jsdocDescriptionWithDot: false,
-	jsdocDescriptionTag: false,
-	jsdocVerticalAlignment: false,
-	jsdocKeepUnParseAbleExampleIndent: false,
+	jsdocDescriptionWithDot: true,
+	jsdocDescriptionTag: DOCUMENTATION_TYPE !== "tsdoc",
+	jsdocVerticalAlignment: true,
+	jsdocKeepUnParseAbleExampleIndent: true,
 	jsdocSingleLineComment: true,
 	jsdocSeparateReturnsFromParam: true,
 	jsdocSeparateTagGroups: true,
@@ -22,7 +22,7 @@ const config = {
 	jsdocCapitalizeDescription: true,
 	jsdocPreferCodeFences: true,
 	jsdocLineWrappingStyle: "greedy",
-	tsdoc: HAS_TYPESCRIPT,
+	tsdoc: HAS_TYPESCRIPT && DOCUMENTATION_TYPE !== "jsdoc",
 } as const;
 
 export default config;
