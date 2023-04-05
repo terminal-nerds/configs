cfce4a37524508f9f61530890279588119835a56
xeho91 <xeho91@pm.me>
2023-04-05 17:10:34 +0800

    feat(Prettier): 🔧  Tweak loading conditions for `jsdoc` plugin

---
+++ .changeset/eight-chefs-occur.md
@@ -0,0 +1,5 @@
+---
+"@terminal-nerds/prettier-config": minor
+---
+
+🔧 Configure `jsdoc` plugin to be dependant on `DOCUMENTATION_TYPE` variable, `jsdoc` or `tsdoc`
--- packages/prettier/tsup.config.ts
+++ packages/prettier/tsup.config.ts
@@ -5,5 +5,5 @@ export default defineConfig((options) => ({
 	...getNodeCJSOptions(options),
 	bundle: true,
 	entry: ["source/index.ts"],
-	noExternal: ["@terminal-nerds/snippets-runtime/module"],
+	noExternal: ["@terminal-nerds/snippets-runtime/module", "@terminal-nerds/snippets-runtime/variable"],
 }));
