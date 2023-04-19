4ae8173bf959947a677985b1e376a2c417c18b87
Mateusz Kadlubowski <xeho91@pm.me>
2023-04-19 15:13:34 +0800

    fix(Stylelint): 🐛 Resolve faulty build config (#312)

---
+++ .changeset/itchy-humans-cheer.md
@@ -0,0 +1,5 @@
+---
+"@terminal-nerds/stylelint-config": patch
+---
+
+🐛 Resolve issue with wrong import (ESM & CJS conflict)
--- packages/stylelint/source/index.ts
+++ packages/stylelint/source/index.ts
@@ -1,4 +1,4 @@
-import { createMergedConfig } from "@terminal-nerds/snippets-config";
+import { createMergedConfig } from "@terminal-nerds/snippets-config/merge";
 import type { Config } from "stylelint";
 
 import { HAS_PRETTIER, HAS_SASS } from "./checks.ts";
