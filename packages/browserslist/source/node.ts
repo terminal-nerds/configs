112f1d1404c723d91de070254d33bd2f356d5a53
xeho91 <xeho91@pm.me>
2023-05-07 20:28:19 +0800

    feat(browserslist): ✨  Add Opera Mini to exlucsion list & ESM support

--- packages/browserslist/source/node.ts
+++ packages/browserslist/source/node.ts
@@ -1,10 +1,6 @@
-// https://browsersl.ist
+/** @see {@link https://browsersl.ist} */
 const config = [
-	/**
-	 * NOTE:
-	 * Meaning LTS version.
-	 * Any version above LTS is not considered as "major release", AFAIK.
-	 */
+	/** NOTE: Meaning LTS version. Any version above LTS is not considered as "major release", AFAIK. */
 	"last 2 node major versions",
 ];
 
