a89c205c9529bd3437759e99d3c806ceafbbfce3
xeho91 <xeho91@pm.me>
2023-05-08 14:38:09 +0800

    chore(ESLint): 🔧  Tweak Tailwind CSS plugin rules

--- packages/eslint/source/plugins/tailwindcss.ts
+++ packages/eslint/source/plugins/tailwindcss.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "eslint-define-config";
 
-// https://github.com/francoismassart/eslint-plugin-tailwindcss
+/** @see {@link https://github.com/francoismassart/eslint-plugin-tailwindcss} */
 const config = defineConfig({
 	extends: ["plugin:tailwindcss/recommended"],
 	plugins: ["tailwindcss"],
+	rules: {
+		// RATIONALE: Let Tailwind CSS official plugin for Prettier take care of sorting
+		"classnames-order": ["off"],
+		// RATIONALE: Conflicts with using CSS modules
+		"no-custom-classname": ["off"],
+	},
 });
 
 export default config;
